ae7d54064dfe2f1447ad514b30d6497e67d520b3
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-25 12:20:55 +0530

    feat: create signin schema using Yup

---
+++ src/schemas/SignInSchema.jsx
@@ -0,0 +1,5 @@
+import * as Yup from "yup";
+export const SignInSchema = Yup.object({
+    email: Yup.string().email("Invalid email").required("Email is required"),
+    password: Yup.string().min(6, "Minimum 6 characters").required("Password is required"),
+})
