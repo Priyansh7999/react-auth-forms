150c09ab82479c1949d07cec65134cc7cc73b1af
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-25 22:05:45 +0530

    feat: display user details on Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { UserContext } from '../context/UserContext'
 
 export default function HomePage() {
-    const navigate = useNavigate()
+    const {user} = useContext(UserContext)
     return (
-        <div className='flex justify-center items-center h-screen gap-4'>
-            <button
-                className="font-sans bg-black hover:bg-gray-800 text-white py-2 px-4 rounded"
-                onClick={() => navigate('/sign-in')}
-            >
-                Sign In
-            </button>
-            <button
-                className="font-sans bg-black hover:bg-gray-800 text-white py-2 px-4 rounded"
-                onClick={() => navigate('/sign-up')}
-            >
-                Sign Up
-            </button>
+        <div className='flex flex-col justify-center items-center h-screen gap-4'>
+            <h1 className='text-4xl font-bold'>Welcome to the Home Page!</h1>
+            <div>
+                <h2 className='text-2xl font-semibold'>User Details:</h2>
+                <p className='text-lg'><strong>Name:</strong> {user.name}</p>
+                <p className='text-lg'><strong>Email:</strong> {user.email}</p>
+            </div>
         </div>
     )
 }
