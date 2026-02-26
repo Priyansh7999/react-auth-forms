32406db20587eaa6739e4ee1d7cfa8c2852590df
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-26 15:19:48 +0530

    feat: add a create-ticket model in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,26 @@
-import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 import Button from '../components/Button'
+import CreateTicketModel from '../models/CreateTicketModel'
 export default function HomePage() {
-    const navigate = useNavigate()
+    const [model, setModel] = useState(false)
+    const handleClick = () => {
+        setModel(!model)
+    }
     return (
-        <div className='flex p-2 justify-center h-screen gap-4'>
-            <h1 className='text-4xl font-bold'>Welcome to the Home Page!</h1>
-            <div>
-                <Button title={"Create Ticket"} onClick={() => navigate('/create-ticket')} />
+        <div className='flex flex-col'>
+            <div className='flex flex-col items-center p-2 h-screen gap-4'>
+                <h1 className='text-4xl font-bold'>Welcome to the Home Page!</h1>
+                <div>
+                    <Button title={"Create Ticket"} onClick={handleClick} />
+                </div>
+            </div>
+            <div className='absolute w-full bg-transparent'>
+                {
+                    model && (
+                        <div className='flex justify-center items-center'> 
+                            <CreateTicketModel handleClose={handleClick} />
+                        </div>)
+                }
             </div>
         </div>
     )
