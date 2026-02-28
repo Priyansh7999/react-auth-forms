a7936eaf587721f2da3051d38a1d411f9262253f
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-28 17:31:21 +0530

    feat:implement viewAllComments service to view all comments on a ticket

--- src/services/commentService.ts
+++ src/services/commentService.ts
@@ -26,5 +26,23 @@ export const addComment = async (ticketId: string, values: AddCommentFormValues)
   if (!response.ok) {
     throw new Error(data?.message || "Failed to add comment");
   }
+  console.log(data)
   return data;
 };
+export const viewAllComments = async(ticketId:string)=>{
+  const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_SERVER_URL}/api/tickets/${ticketId}/comments`,
+        {
+            method: 'GET',
+            headers: {
+                ...getAuthHeader()
+            }
+        }
+    );
+    const data = await response.json();
+    if (!response.ok) {
+        throw new Error(data?.message || 'Failed to create ticket');
+    }
+    console.log(data)
+    return data;
+}
